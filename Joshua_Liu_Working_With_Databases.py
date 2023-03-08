e0f5acbf04c4671b3a5eadfc3628c03f9f846d70
shitass <joshua0307lol@gmail.com>
2023-03-08 08:51:06 -0600

    Made PEP8 compliant

--- Joshua_Liu_Working_With_Databases.py
+++ Joshua_Liu_Working_With_Databases.py
@@ -1,39 +1,39 @@
 Marvel_Superheros = {
-  "Spiderman" : {
-           "Name" : "Peter Parker",
-           "Weapons" : ["Webbing"],
-           "Super Powers" : ["Speed", "Reflexes",
-                             "Spider-Sense"],
-           "Weaknesses" : ["Ethyl Chloride Pesticide"] },
+  "Spiderman": {
+           "Name": "Peter Parker",
+           "Weapons": ["Webbing"],
+           "Super Powers": ["Speed", "Reflexes",
+                            "Spider-Sense"],
+           "Weaknesses": ["Ethyl Chloride Pesticide"]},
   "Thing" : {
            "Name": "Ben Grimm",
            "Weapons": ["Fists"],
            "Powers": ["Immortal", "Super Strength",
                       "Enhanced Lung Capacity", "Good Fighter"],
-         "Weakness": [None] },  
+           "Weakness": [None]},
   "Baby_Groot" : {
-           "Name" : "Baby Groot",
-           "Weapons" : ["Strong Branches"],
-           "Super Powers" : ["Self-Healing", "Controls Plant",
+           "Name": "Baby Groot",
+           "Weapons": ["Strong Branches"],
+           "Super Powers": ["Self-Healing", "Controls Plant",
                             "Immune to Fire"],
-           "Weaknesses" : ["Termites"] },
-  "Ironman" : {
-           "Name" : "Tony Stark",
-           "Weapons" : ["Ironman Suit", "AI", "Blasters"],
-           "Superpowers" : ["Smart", "Rich"],
-           "Weaknesses" : ["Arrogant"] },
-  "Deadpool" : {
-           "Name" : "Wade Wilson",
-           "Weapons" : ["Katanas", "Grenades", "Guns"],
-           "Super Powers" : ["Speed", "Bullet Proof",
-                             "Self-Healing"],
-           "Weaknesses" : ["Women", "Ugly"] },
-  "Antman" : {
-           "Name" : "Scott Lang",
-           "Weapons" : ["Antman Suit"],
-           "Super Powers" : ["Communication with Insects",
-                             "Sound Amplification"],
-           "Weaknesses" : ["Lacks Ability Control"]}
+           "Weaknesses": ["Termites"]},
+  "Ironman": {
+           "Name": "Tony Stark",
+           "Weapons": ["Ironman Suit", "AI", "Blasters"],
+           "Superpowers": ["Smart", "Rich"],
+           "Weaknesses": ["Arrogant"]},
+  "Deadpool": {
+           "Name": "Wade Wilson",
+           "Weapons": ["Katanas", "Grenades", "Guns"],
+           "Super Powers": ["Speed", "Bullet Proof",
+                            "Self-Healing"],
+           "Weaknesses": ["Women", "Ugly"] },
+  "Antman": {
+           "Name": "Scott Lang",
+           "Weapons": ["Antman Suit"],
+           "Super Powers": ["Communication with Insects",
+                            "Sound Amplification"],
+           "Weaknesses": ["Lacks Ability Control"]}
 }
 
 for key in Marvel_Superheros:
