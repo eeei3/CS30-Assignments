032411d24a03b1f4af0b4f0b2f8dfcf43fbecffd
shitass <joshua0307lol@gmail.com>
2023-03-08 08:46:13 -0600

    Created the Working With Databases assignment and completed it.

---
+++ Joshua_Liu_Working_With_Databases.py
@@ -0,0 +1,48 @@
+Marvel_Superheros = {
+  "Spiderman" : {
+           "Name" : "Peter Parker",
+           "Weapons" : ["Webbing"],
+           "Super Powers" : ["Speed", "Reflexes",
+                             "Spider-Sense"],
+           "Weaknesses" : ["Ethyl Chloride Pesticide"] },
+  "Thing" : {
+           "Name": "Ben Grimm",
+           "Weapons": ["Fists"],
+           "Powers": ["Immortal", "Super Strength",
+                      "Enhanced Lung Capacity", "Good Fighter"],
+         "Weakness": [None] },  
+  "Baby_Groot" : {
+           "Name" : "Baby Groot",
+           "Weapons" : ["Strong Branches"],
+           "Super Powers" : ["Self-Healing", "Controls Plant",
+                            "Immune to Fire"],
+           "Weaknesses" : ["Termites"] },
+  "Ironman" : {
+           "Name" : "Tony Stark",
+           "Weapons" : ["Ironman Suit", "AI", "Blasters"],
+           "Superpowers" : ["Smart", "Rich"],
+           "Weaknesses" : ["Arrogant"] },
+  "Deadpool" : {
+           "Name" : "Wade Wilson",
+           "Weapons" : ["Katanas", "Grenades", "Guns"],
+           "Super Powers" : ["Speed", "Bullet Proof",
+                             "Self-Healing"],
+           "Weaknesses" : ["Women", "Ugly"] },
+  "Antman" : {
+           "Name" : "Scott Lang",
+           "Weapons" : ["Antman Suit"],
+           "Super Powers" : ["Communication with Insects",
+                             "Sound Amplification"],
+           "Weaknesses" : ["Lacks Ability Control"]}
+}
+
+for key in Marvel_Superheros:
+    print(key)
+
+for key in Marvel_Superheros:
+    print(Marvel_Superheros[key]["Name"])
+
+for key in Marvel_Superheros:
+    hero = f"Hero Title: {key}"
+    name = f'Name: {Marvel_Superheros[key]["Name"]}'
+    print("{:<12} {:<12}".format(hero, name))
